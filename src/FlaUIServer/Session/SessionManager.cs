3f7a1cd5b51ff659f8726bd0c2096504a087d2c7
Pavel Juza <>
2024-05-27 15:16:34 +0200

    Implemented timed out session cleaning service

--- src/FlaUIServer/Session/SessionManager.cs
+++ src/FlaUIServer/Session/SessionManager.cs
@@ -6,6 +6,7 @@ namespace FlaUIServer.Session;
 
 public class SessionManager : ISessionManager
 {
+    private const int InactiveTimeout = 90;
     private readonly ConcurrentDictionary<Guid, WinAppSession> _sessions = new ();
 
     /// <inheritdoc />
@@ -53,5 +54,19 @@ public class SessionManager : ISessionManager
     public SessionInfo[] GetSessions()
     {
         return _sessions.Select(x => new SessionInfo { SessionId = x.Key, Created = x.Value.Created, LastActionAt = x.Value.LastActionAt}).ToArray();
+    }
+
+    /// <inheritdoc />
+    public Guid[] RemoveInactive()
+    {
+        var deleted = new List<Guid>();
+        var orphanedSession = _sessions.Where(x => x.Value.LastActionAt.AddSeconds(InactiveTimeout) < DateTimeOffset.Now).ToArray();
+        foreach (var session in orphanedSession)
+        {
+            DeleteSession(session.Key);
+            deleted.Add(session.Key);
+        }
+        
+        return deleted.ToArray();
     }
 }
